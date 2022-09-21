f7414b3fc9d6c326c9ff34ecea87e4ee0a0bedd8
mubzie <rabiumubarak92@gmail.com>
2022-09-21 09:36:20 +0100

    completed basic functionality. to-do: fix final winner result

--- dist/modules/rps.js
+++ dist/modules/rps.js
@@ -2,45 +2,57 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ps = require("prompt-sync");
 const prompt = ps();
-const gameCount = 5;
-const getPlayerChoice = () => {
-    const playerChoice = prompt('what is your choice? rock, paper or scissor?', '');
-    console.log(playerChoice);
-    return playerChoice;
-};
-const player = getPlayerChoice();
-const getComputerChoice = () => {
-    const computerChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(computerChoice);
-    if (computerChoice === 1) {
-        console.log('rock');
-        return 'rock';
-    }
-    else if (computerChoice === 2) {
-        console.log('paper');
-        return 'paper';
-    }
-    else {
-        console.log('scissor');
-        return 'scissor';
-    }
-};
-const computer = getComputerChoice();
-const gamePlay = (pick1, pick2) => {
-    if (pick1 === 'rock' && pick2 === 'paper') {
+let playerWin = 0;
+let computerWin = 0;
+const game = () => {
+    const getPlayerChoice = () => {
+        const playerChoice = prompt('what is your choice? rock, paper or scissor?', '');
+        console.log(playerChoice);
+        return playerChoice;
+    };
+    const player = getPlayerChoice();
+    const getComputerChoice = () => {
+        const computerChoice = Math.floor(Math.random() * 3) + 1;
+        console.log(computerChoice);
+        if (computerChoice === 1) {
+            console.log('rock');
+            return 'rock';
+        }
+        else if (computerChoice === 2) {
+            console.log('paper');
+            return 'paper';
+        }
+        else {
+            console.log('scissor');
+            return 'scissor';
+        }
+    };
+    const computer = getComputerChoice();
+    if (player === 'rock' && computer === 'paper') {
+        playerWin++;
         console.log('computer wins');
     }
-    else if (pick1 === 'rock' && pick2 === 'scissor') {
+    else if (player === 'rock' && computer === 'scissor') {
+        computerWin++;
         console.log('player wins');
     }
     else {
         console.log('what a hit');
     }
 };
-const game = () => {
-    for (let i = 1; i <= gameCount; i++) {
-        gamePlay(player, computer);
+const play = () => {
+    for (let i = 1; i <= 5; i++) {
+        game();
+    }
+    if (playerWin > computerWin) {
+        console.log('player wins the game');
+    }
+    else if (playerWin < computerWin) {
+        console.log('computer wins the game');
+    }
+    else {
+        console.log('the game ended in a tie');
     }
 };
-game();
+play();
 //# sourceMappingURL=rps.js.map
--- src/modules/rps.ts
+++ src/modules/rps.ts
@@ -5,50 +5,70 @@ const ps = require("prompt-sync");
 const prompt = ps();
 //*******************************/
 
-type Choice = string | null;
 
-const gameCount: number = 5;
-
-const getPlayerChoice = (): Choice => {
-    const playerChoice: Choice = prompt('what is your choice? rock, paper or scissor?', '');
-    console.log(playerChoice);
-
-    return playerChoice;
-}
-
-const player = getPlayerChoice();
-
-const getComputerChoice = (): string => {
-    const computerChoice: number = Math.floor(Math.random() * 3) + 1; 
-    console.log(computerChoice);
-    if (computerChoice === 1) {
-        console.log('rock')
-        return 'rock'
-    } else if (computerChoice === 2) {
-        console.log('paper')
-        return 'paper'
-    } else {
-        console.log('scissor')
-        return 'scissor'
-    }
-}
-
-const computer = getComputerChoice();
-
-const gamePlay = (pick1: unknown, pick2: unknown) => {
-    if (pick1 === 'rock' && pick2 === 'paper') {
-        console.log('computer wins');
-    } else if (pick1 === 'rock' && pick2 === 'scissor') {
-        console.log('player wins');
-    } else {
-        console.log('what a hit')
-    }
-}
+let playerWin: number = 0;
+let computerWin: number = 0;
 
 const game = () => {
-    for (let i = 1; i <= gameCount; i++) {
-         gamePlay(player, computer);
+    
+    type Choice = string | null;
+
+    const getPlayerChoice = (): Choice => {
+    
+        const playerChoice: Choice = prompt('what is your choice? rock, paper or scissor?', '');
+        console.log(playerChoice);
+    
+        return playerChoice;
     }
+    
+    const player = getPlayerChoice();
+    
+    const getComputerChoice = (): string => {
+    
+        const computerChoice: number = Math.floor(Math.random() * 3) + 1; 
+        console.log(computerChoice);
+        if (computerChoice === 1) {
+            console.log('rock')
+            return 'rock'
+        } else if (computerChoice === 2) {
+            console.log('paper')
+            return 'paper'
+        } else {
+            console.log('scissor')
+            return 'scissor'
+        }
+    
+    }
+    
+    const computer = getComputerChoice();
+    
+    if (player === 'rock' && computer === 'paper') {
+        playerWin++;
+        console.log('computer wins');
+    } else if (player === 'rock' && computer === 'scissor') {
+        computerWin++;
+        console.log('player wins');
+    } else {
+        console.log('what a hit');
+    }
+    
 }
 
-game()
+const play = () => {
+    
+    for (let i = 1; i <= 5; i++) {
+        game();
+    }
+
+    if (playerWin > computerWin) {
+        console.log('player wins the game');
+    } else if (playerWin < computerWin) {
+        console.log('computer wins the game');
+    } else {
+        console.log('the game ended in a tie');
+    }
+
+}
+
+
+play()
